2f55beea31c84b5aba65f6b6fd039b9b7b814580
Niraj Ghetiya <104854542+niraj003@users.noreply.github.com>
2024-01-11 10:14:01 +0530

    Create HashSet.cs

---
+++ RVD/Fundamentals/HashSet/HashSet.cs
@@ -0,0 +1,6 @@
+Write a program to create two HashSets with integer dataand simulate the following functions:
+1) Intersection 
+2) Union
+3) ExceptWith
+(Do not use the inbuilt functions provided for them) 
+
